84acb797396569a242ca81615d40da1d63a8229f
Yaoharee Lahtee <127247155+morrocwi@users.noreply.github.com>
2026-09-10 19:59:48 +0700

    Export OpenFOAM adapter API

--- software/observableflow/observableflow/__init__.py
+++ software/observableflow/observableflow/__init__.py
@@ -2,9 +2,17 @@ from .core import Sensor, AnalysisResult, observability_matrix, analyze_design, 
 from .optimize import pareto_optimize, greedy_optimize
 from .linearize import finite_difference_jacobian, linearize_trajectory
 from .ns_bounds import cubic_galerkin_dimension, shell_values, shell_count, scalar_min_depth, shell_min_depth
+from .openfoam import (
+    ProbeDataset, SnapshotDataset, PODReduction, OpenFOAMObservableModel,
+    load_probes, load_numeric_snapshots, pod_reduce_snapshots,
+    fit_lti_probe_model, fit_openfoam_case, generate_probes_function_object,
+)
 
 __all__ = [
     "Sensor", "AnalysisResult", "observability_matrix", "analyze_design", "svd_metrics",
     "pareto_optimize", "greedy_optimize", "finite_difference_jacobian", "linearize_trajectory",
     "cubic_galerkin_dimension", "shell_values", "shell_count", "scalar_min_depth", "shell_min_depth",
+    "ProbeDataset", "SnapshotDataset", "PODReduction", "OpenFOAMObservableModel",
+    "load_probes", "load_numeric_snapshots", "pod_reduce_snapshots",
+    "fit_lti_probe_model", "fit_openfoam_case", "generate_probes_function_object",
 ]
